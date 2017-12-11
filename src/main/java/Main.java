bcb402bd52cc57b44724ee0aec0fd5bcd2404da6
Kanoorus <Kanoorus@gmail.com>
2017-12-11 17:27:41 +0700

    Задание 1 для лекции 4 Исправления логики работы 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,20 +9,31 @@ public class Main {
         printMass(mass);
         int min = 0;
         int max = 0;
-        for (int i = 0; i < 20; i++) {
-            if (mass[i]>=0 && mass[i]<mass[max]){
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i]>mass[max]){
                 max=i;
             }
-            if (mass[i]<0 && mass[i]>mass[min]){
+            if (mass[i]<mass[min]){
                 min=i;
             }
-
         }
-        System.out.println("\nМинимальное число в массиве: "+ mass[min] + "\nмаксимально числов массиве: " + mass[max]);
-        int temp=mass[min];
-        mass[min]=mass[max];
-        mass[max]=temp;
-        printMass(mass);
+        if (mass[min]>=0||mass[max]<=0){
+            System.out.println("Сгенерированный массив не удовлетворяет условиям");
+        } else {
+            for (int i = 0; i < mass.length; i++) {
+                if (mass[max]>mass[i]&&mass[i]>0){
+                    max=i;
+                }
+                if (mass[min]<mass[i]&&mass[i]<0){
+                    min=i;
+                }
+            }
+            System.out.println("\nМинимальное число в массиве: "+ mass[min] + "\nмаксимально числов массиве: " + mass[max]);
+            int temp=mass[min];
+            mass[min]=mass[max];
+            mass[max]=temp;
+            printMass(mass);
+        }
     }
 
     public static int getRand() {
